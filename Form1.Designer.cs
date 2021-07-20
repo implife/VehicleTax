235ed359893cfc0b155add9b0b7b31b0ed8c602e
Zhian_PC <implife10512@gmail.com>
2021-07-20 15:58:23 +0800

    First commit, add all the files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,308 @@
+﻿
+namespace VehicleLicenseTaxCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbtnRange = new System.Windows.Forms.RadioButton();
+            this.rbtnYear = new System.Windows.Forms.RadioButton();
+            this.calendarPanel = new System.Windows.Forms.Panel();
+            this.dtPickerTo = new System.Windows.Forms.DateTimePicker();
+            this.dtPickerFrom = new System.Windows.Forms.DateTimePicker();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cbxType = new System.Windows.Forms.ComboBox();
+            this.cbxDisplacement = new System.Windows.Forms.ComboBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.calendarPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(97, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "使用期間";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(139, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "用途";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(6, 178);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(187, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "汽缸cc數/馬達馬力";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(62, 249);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "試算結果";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbtnRange);
+            this.groupBox1.Controls.Add(this.rbtnYear);
+            this.groupBox1.Location = new System.Drawing.Point(216, 38);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(210, 58);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "請選擇";
+            // 
+            // rbtnRange
+            // 
+            this.rbtnRange.AutoSize = true;
+            this.rbtnRange.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtnRange.Location = new System.Drawing.Point(114, 19);
+            this.rbtnRange.Name = "rbtnRange";
+            this.rbtnRange.Size = new System.Drawing.Size(75, 24);
+            this.rbtnRange.TabIndex = 1;
+            this.rbtnRange.TabStop = true;
+            this.rbtnRange.Text = "依期間";
+            this.rbtnRange.UseVisualStyleBackColor = true;
+            this.rbtnRange.CheckedChanged += new System.EventHandler(this.rbtnRange_CheckedChanged);
+            // 
+            // rbtnYear
+            // 
+            this.rbtnYear.AutoSize = true;
+            this.rbtnYear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtnYear.Location = new System.Drawing.Point(19, 20);
+            this.rbtnYear.Name = "rbtnYear";
+            this.rbtnYear.Size = new System.Drawing.Size(75, 24);
+            this.rbtnYear.TabIndex = 0;
+            this.rbtnYear.TabStop = true;
+            this.rbtnYear.Text = "全年度";
+            this.rbtnYear.UseVisualStyleBackColor = true;
+            this.rbtnYear.CheckedChanged += new System.EventHandler(this.rbtnYear_CheckedChanged);
+            // 
+            // calendarPanel
+            // 
+            this.calendarPanel.Controls.Add(this.dtPickerTo);
+            this.calendarPanel.Controls.Add(this.dtPickerFrom);
+            this.calendarPanel.Controls.Add(this.label6);
+            this.calendarPanel.Controls.Add(this.label5);
+            this.calendarPanel.Location = new System.Drawing.Point(432, 25);
+            this.calendarPanel.Name = "calendarPanel";
+            this.calendarPanel.Size = new System.Drawing.Size(239, 84);
+            this.calendarPanel.TabIndex = 5;
+            // 
+            // dtPickerTo
+            // 
+            this.dtPickerTo.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtPickerTo.CustomFormat = "           yyyy - MM - dd";
+            this.dtPickerTo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtPickerTo.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtPickerTo.Location = new System.Drawing.Point(43, 50);
+            this.dtPickerTo.Name = "dtPickerTo";
+            this.dtPickerTo.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.dtPickerTo.Size = new System.Drawing.Size(168, 22);
+            this.dtPickerTo.TabIndex = 3;
+            // 
+            // dtPickerFrom
+            // 
+            this.dtPickerFrom.CustomFormat = "           yyyy - MM - dd";
+            this.dtPickerFrom.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtPickerFrom.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtPickerFrom.Location = new System.Drawing.Point(43, 18);
+            this.dtPickerFrom.Name = "dtPickerFrom";
+            this.dtPickerFrom.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.dtPickerFrom.Size = new System.Drawing.Size(168, 22);
+            this.dtPickerFrom.TabIndex = 2;
+            this.dtPickerFrom.ValueChanged += new System.EventHandler(this.dtPickerFrom_ValueChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(12, 51);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(25, 20);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "至";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 19);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(25, 20);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "從";
+            // 
+            // cbxType
+            // 
+            this.cbxType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbxType.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbxType.FormattingEnabled = true;
+            this.cbxType.Items.AddRange(new object[] {
+            "自用小客車",
+            "營業用小客車",
+            "大客車",
+            "貨車",
+            "機車"});
+            this.cbxType.Location = new System.Drawing.Point(216, 114);
+            this.cbxType.Name = "cbxType";
+            this.cbxType.Size = new System.Drawing.Size(210, 28);
+            this.cbxType.TabIndex = 6;
+            this.cbxType.SelectedIndexChanged += new System.EventHandler(this.cbxType_SelectedIndexChanged);
+            // 
+            // cbxDisplacement
+            // 
+            this.cbxDisplacement.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbxDisplacement.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbxDisplacement.FormattingEnabled = true;
+            this.cbxDisplacement.Location = new System.Drawing.Point(216, 178);
+            this.cbxDisplacement.Name = "cbxDisplacement";
+            this.cbxDisplacement.Size = new System.Drawing.Size(210, 28);
+            this.cbxDisplacement.TabIndex = 4;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.Location = new System.Drawing.Point(174, 249);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(19, 20);
+            this.lblResult.TabIndex = 7;
+            this.lblResult.Text = "--";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(462, 159);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(86, 47);
+            this.btnCalculate.TabIndex = 8;
+            this.btnCalculate.Text = "試算";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(585, 159);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(86, 47);
+            this.btnReset.TabIndex = 9;
+            this.btnReset.Text = "重填";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResult.Location = new System.Drawing.Point(129, 277);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtResult.Size = new System.Drawing.Size(514, 245);
+            this.txtResult.TabIndex = 10;
+            this.txtResult.Text = " ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(688, 578);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.cbxDisplacement);
+            this.Controls.Add(this.cbxType);
+            this.Controls.Add(this.calendarPanel);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = " ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.calendarPanel.ResumeLayout(false);
+            this.calendarPanel.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbtnRange;
+        private System.Windows.Forms.RadioButton rbtnYear;
+        private System.Windows.Forms.Panel calendarPanel;
+        private System.Windows.Forms.DateTimePicker dtPickerFrom;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker dtPickerTo;
+        private System.Windows.Forms.ComboBox cbxType;
+        private System.Windows.Forms.ComboBox cbxDisplacement;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.TextBox txtResult;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace VehicleLicenseTaxCalculator
+{
+    public partial class Form1 : Form
+    {
+        #region 資料設定與宣告
+
+        // 小客車資料
+        private string[] displacementCarAry = {
+            "500cc 以下 / 38HP以下(38.6PS以下)", "501cc - 600cc / 38.1-56HP(38.7-56.8PS)", 
+            "601cc - 1200cc / 56.1-83HP(56.9-84.2PS)", "1201cc - 1800cc / 83.1-182HP(84.3-184.7PS)",
+            "1801cc - 2400cc / 182.1-262HP(184.8-265.9PS)", "2401cc - 3000cc / 262.1-322HP(266-326.8PS)",
+            "3001cc - 4200cc / 322.1-414HP(326.9-420.2PS", "4201cc - 5400cc / 414.1-469HP(420.3-476.0PS)",
+            "5401cc - 6600cc / 469.1-509HP(476.1-516.6PS)", "6601cc - 7800cc / 509.1HP以上(516.7PS以上)", 
+            "7801cc以上"
+        };
+        private int[] selfCarTaxAry = { 1620, 2160, 4320, 7120, 11230, 15210, 28220, 46170, 69690, 117000, 151200 };
+        private int[] businessCarTaxAry = { 900, 1260, 2160, 3060, 6480, 9900, 16380, 24300, 33660, 44460, 56700 };
+
+        // 大客車和貨車資料
+        private string[] displacementBigCarAry = {
+            "500cc 以下", "501cc - 600cc", "601cc - 1200cc", "1201cc - 1800cc",
+            "1801cc - 2400cc", "2401cc - 3000cc / 138HP以下(140.1PS以下)", "3001cc - 3600cc",
+            "3601cc - 4200cc / 138.1-200HP(140.2-203.0PS)", "4201cc - 4800cc", 
+            "4801cc - 5400cc / 200.1-247HP(203.1-250.7PS)", "5401cc - 6000cc",
+            "6001cc - 6600cc / 247.1-286HP(250.8-290.3PS)","6601cc - 7200cc", 
+            "7201cc - 7800cc / 286.1-336HP(290.4-341.0PS)", "7801cc - 8400cc",
+            "8401cc - 9000cc / 336.1-361HP(341.1-366.4PS)","9001cc - 9600cc", 
+            "9601cc - 10200cc / 361.1HP以上(366.5PS以上)", "10201以上"
+        };
+        private int[] bigCarTaxAry = {
+            0, 1080, 1800, 2700, 3600, 4500, 5400, 6300, 7200, 8100,
+            9000, 9900, 10800, 11700, 12600, 13500, 14400, 15300, 16200
+        };
+        private int[] truckTaxAry = {
+            900, 1080, 1800, 2700, 3600, 4500, 5400, 6300, 7200, 8100,
+            9000, 9900, 10800, 11700, 12600, 13500, 14400, 15300, 16200
+        };
+
+        // 機車資料
+        private string[] displacementMotorAry = {
+            "150cc 以下 / 12HP以下(12.2PS以下)", "151cc - 250cc / 12.1 - 20HP(12.3 - 20.3PS)",
+            "251cc - 500cc / 20.1HP以上(20.4PS以上)", "501cc - 600cc",
+            "601cc - 1200cc", "1201cc - 1800cc", "1801cc以上"
+        };
+        private int[] motorTaxAry = { 0, 800, 1620, 2160, 3600, 4320, 7120, 11230 };
+
+        // 各種車型字典資料宣告
+        private Dictionary<string, int> selfUseCarDic = new Dictionary<string, int>();
+        private Dictionary<string, int> businessUseCarDic = new Dictionary<string, int>();
+        private Dictionary<string, int> bigCarDic = new Dictionary<string, int>();
+        private Dictionary<string, int> truckDic = new Dictionary<string, int>();
+        private Dictionary<string, int> motorDic = new Dictionary<string, int>();
+
+        #endregion
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region Event
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.init();
+        }
+
+        private void rbtnYear_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbtnYear.Checked)
+                this.calendarPanel.Visible = false;
+        }
+
+        private void rbtnRange_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbtnRange.Checked)
+                this.calendarPanel.Visible = true;
+        }
+
+        private void cbxType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (cbxType.SelectedItem)
+            {
+                case "自用小客車":
+                    this.cbxDisplacement.Items.Clear();
+                    this.cbxDisplacement.Items.AddRange(displacementCarAry);
+                    break;
+                case "營業用小客車":
+                    this.cbxDisplacement.Items.Clear();
+                    this.cbxDisplacement.Items.AddRange(displacementCarAry);
+                    break;
+                case "大客車":
+                    this.cbxDisplacement.Items.Clear();
+                    this.cbxDisplacement.Items.AddRange(displacementBigCarAry);
+                    break;
+                case "貨車":
+                    this.cbxDisplacement.Items.Clear();
+                    this.cbxDisplacement.Items.AddRange(displacementBigCarAry);
+                    break;
+                case "機車":
+                    this.cbxDisplacement.Items.Clear();
+                    this.cbxDisplacement.Items.AddRange(displacementMotorAry);
+                    break; 
+            }
+            this.cbxDisplacement.SelectedIndex = 0;
+        }
+
+        private void dtPickerFrom_ValueChanged(object sender, EventArgs e)
+        {
+            // 結束日期不可比開始日期前面
+            dtPickerTo.MinDate = dtPickerFrom.Value;
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            string type = cbxType.SelectedItem.ToString();
+            string displacement = cbxDisplacement.SelectedItem.ToString();
+
+            // 藉由用途類型和汽缸數取得對應的年額度
+            int tax = getTax(type, displacement);
+            
+
+            // -------- 取得日期、計算天數和結果 --------
+            DateTime dateFrom, dateTo;
+            if (rbtnYear.Checked)
+            {
+                int thisYear = DateTime.Now.Year;
+                dateFrom = new DateTime(thisYear, 1, 1);
+                dateTo = new DateTime(thisYear, 12, 31);
+            }
+            else
+            {
+                dateFrom = dtPickerFrom.Value;
+                dateTo = dtPickerTo.Value;
+            }
+
+            // 處理可能有跨年份的問題
+            int count = dateTo.Year - dateFrom.Year + 1;    // 跨年份的數量
+            int[] intervalYears = new int[count];           // 跨的年份陣列 ex.{2020, 2021, 2022} 
+            int[] totalDaysInYear = new int[count];         // 每一年的天數，可能有潤年
+            DateTime[,] intervals = new DateTime[count, 2]; // 每一年的天數區間
+            int[] days = new int[count];                    // 根據天數區間算出的天數
+            int[] results = new int[count];                 // 根據天數算出的每年稅額
+
+            for(int i = 0; i < count; i++)
+            {
+                intervalYears[i] = dateFrom.Year + i;
+
+                // 判斷閏年
+                if (DateTime.IsLeapYear(dateFrom.Year + i))
+                    totalDaysInYear[i] = 366; 
+                else
+                    totalDaysInYear[i] = 365;
+
+                // 每年的起始日
+                if (i == 0)
+                    intervals[i, 0] = dateFrom;
+                else
+                    intervals[i, 0] = new DateTime(dateFrom.Year + i, 1, 1);
+                
+                // 每年的結束日
+                if (i == count - 1)
+                    intervals[i, 1] = dateTo;
+                else
+                    intervals[i, 1] = new DateTime(dateFrom.Year + i, 12, 31);
+            }
+            for (int i = 0; i <= intervals.GetUpperBound(0); i++)
+            {
+                days[i] = (intervals[i, 1] - intervals[i, 0])
+                    .Add(new TimeSpan(1, 0, 0, 1)).Days;    // 頭尾都要算所以加一天，加一秒避免預設的誤差
+                results[i] = (int)(tax * days[i] / totalDaysInYear[i]); // 小數部分捨棄
+            }
+
+            // 將結果寫成字串
+            string resultText = "";
+            for (int i = 0; i < count; i++)
+            {
+                resultText +=
+                    $"使用期間: {intervals[i, 0].ToString("yyyy-MM-dd")} ~ {intervals[i, 1].ToString("yyyy-MM-dd")}"
+                        + Environment.NewLine +
+                    $"使用天數: {days[i]} 天" + Environment.NewLine +
+                    $"用途: {type}" + Environment.NewLine +
+                    $"汽缸cc數: {displacement}" + Environment.NewLine +
+                    $"計算公式: {tax} * {days[i]} / {totalDaysInYear[i]} = {results[i]} 元" + Environment.NewLine +
+                    $"應納稅額: {results[i]}" + Environment.NewLine + Environment.NewLine;
+            }
+            // -------- 取得日期、計算天數和結果 --------
+
+            if (count > 1)
+            {
+                int total = 0;
+                foreach (int item in results)
+                    total += item;
+                resultText += $"全部應納稅額: {total} 元" + Environment.NewLine + Environment.NewLine;
+            }
+            txtResult.Text = resultText;
+            
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            initTheComponent();
+        }
+
+        #endregion
+
+        #region Custom method
+
+        private void init()
+        {
+            // 車型資料加入字典
+            for (int i = 0; i < displacementCarAry.Length; i++)
+            {
+                selfUseCarDic.Add(displacementCarAry[i], selfCarTaxAry[i]);
+                businessUseCarDic.Add(displacementCarAry[i], businessCarTaxAry[i]);
+            }
+            for (int i = 0; i < displacementBigCarAry.Length; i++)
+            {
+                bigCarDic.Add(displacementBigCarAry[i], bigCarTaxAry[i]);
+                truckDic.Add(displacementBigCarAry[i], truckTaxAry[i]);
+            }
+            for (int i = 0; i < displacementMotorAry.Length; i++)
+            {
+                motorDic.Add(displacementMotorAry[i], motorTaxAry[i]);
+            }
+
+            initTheComponent();
+        }
+        private void initTheComponent()
+        {
+            this.calendarPanel.Visible = false;
+            this.cbxType.SelectedIndex = 0;
+            this.cbxDisplacement.SelectedIndex = 0;
+            this.rbtnYear.Checked = true;
+            this.dtPickerFrom.Value = DateTime.Now;
+            this.dtPickerTo.MinDate = DateTime.Now; // 結束日期不可比起始日期小
+            this.txtResult.Text = "";
+            this.cbxDisplacement.DropDownWidth = 360;
+        }
+
+        private int getTax(string type, string displacement)
+        {
+            switch (type)
+            {
+                case "自用小客車":
+                    return selfUseCarDic[displacement];
+                case "營業用小客車":
+                    return businessUseCarDic[displacement];
+                case "大客車":
+                    return bigCarDic[displacement];
+                case "貨車":
+                    return truckDic[displacement];
+                case "機車":
+                    return motorDic[displacement];
+                default:
+                    return 0;
+            }
+        }
+
+       // private 
+
+        #endregion
+    }
+}
